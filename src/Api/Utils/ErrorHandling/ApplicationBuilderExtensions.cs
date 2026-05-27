4d6d7520a47ab6dbcaa8a0be39ca8ebe00a4275e
Tim Peel <tim.peel@equalexperts.com>
2026-05-27 09:37:53 +0100

    Move error handling setup into extension method

---
+++ src/Api/Utils/ErrorHandling/ApplicationBuilderExtensions.cs
@@ -0,0 +1,65 @@
+using Defra.WasteObligations.Api.Data;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Defra.WasteObligations.Api.Utils.ErrorHandling;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void UseErrorHandling(this IApplicationBuilder app)
+    {
+        app.UseExceptionHandler(
+            new ExceptionHandlerOptions
+            {
+                AllowStatusCode404Response = true,
+                ExceptionHandler = async context =>
+                {
+                    var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var error = exceptionHandlerFeature?.Error;
+                    var problemDetails = new ProblemDetails();
+
+                    switch (error)
+                    {
+                        case BadHttpRequestException badHttpRequestException:
+                            context.Response.StatusCode = badHttpRequestException.StatusCode;
+                            problemDetails.Title = "Bad request";
+                            problemDetails.Detail = badHttpRequestException.Message;
+                            problemDetails.Status = badHttpRequestException.StatusCode;
+                            break;
+
+                        case EntityException entityException:
+                            context.Response.StatusCode = StatusCodes.Status409Conflict;
+                            problemDetails.Title = "Conflict occurred";
+                            problemDetails.Detail = entityException.Message;
+                            problemDetails.Status = StatusCodes.Status409Conflict;
+                            break;
+
+                        case ConcurrencyException concurrencyException:
+                            context.Response.StatusCode = StatusCodes.Status422UnprocessableEntity;
+                            problemDetails.Title = "Concurrency conflict";
+                            problemDetails.Detail = concurrencyException.Message;
+                            problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                            break;
+
+                        default:
+                            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                            problemDetails.Title = "An error occurred while processing your request.";
+                            problemDetails.Status = StatusCodes.Status500InternalServerError;
+                            break;
+                    }
+
+                    await context
+                        .RequestServices.GetRequiredService<IProblemDetailsService>()
+                        .WriteAsync(
+                            new ProblemDetailsContext
+                            {
+                                HttpContext = context,
+                                AdditionalMetadata = exceptionHandlerFeature?.Endpoint?.Metadata,
+                                ProblemDetails = problemDetails,
+                            }
+                        );
+                },
+            }
+        );
+    }
+}
