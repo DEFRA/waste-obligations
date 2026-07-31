c75ef7256719c0b9c8f0be0b6d242eec49f1e5a8
Tim Peel <tim.peel@equalexperts.com>
2026-07-31 17:03:38 +0100

    MO-381: Add health check that will succeed for authenticated clients (#157)

--- tests/Api.Tests/Endpoints/Health/HealthTests.cs
+++ tests/Api.Tests/Endpoints/Health/HealthTests.cs
@@ -7,6 +7,32 @@ public class HealthTests(ApiWebApplicationFactory factory, ITestOutputHelper out
     : EndpointTestBase(factory, outputHelper)
 {
     [Fact]
+    public async Task AuthorizedHealth_WhenUnauthenticated_ShouldBeUnauthorized()
+    {
+        var client = CreateClient(addAuthorizationHeader: false);
+
+        var response = await client.GetAsync(
+            Testing.Endpoints.Health.Authorized(),
+            TestContext.Current.CancellationToken
+        );
+
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task AuthorizedHealth_WhenAuthenticated_ShouldBeOk()
+    {
+        var client = CreateClient();
+
+        var response = await client.GetAsync(
+            Testing.Endpoints.Health.Authorized(),
+            TestContext.Current.CancellationToken
+        );
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
     public async Task Health_ShouldBeOk()
     {
         var client = CreateClient();
